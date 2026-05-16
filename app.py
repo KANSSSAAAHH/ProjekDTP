d967fe870463415af625f211f7241b50ede4f741
KHANSSSAAAHH <ramazankhanza@gmail.com>
2026-05-16 13:08:01 +0700

    auto train model in app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 import json
 import pickle
@@ -5,9 +6,69 @@ import numpy as np
 import nltk
 import random
 from nltk.stem import WordNetLemmatizer
-from keras.models import load_model
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout
+from keras.optimizers import SGD
+
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('punkt_tab')
 
 lemmatizer = WordNetLemmatizer()
+
+# Auto-train jika model belum ada
+if not os.path.exists('chatbot_model.h5'):
+    intents = json.loads(open('chatbot.json').read())
+    words = []
+    classes = []
+    documents = []
+    ignore_letters = ['?', '!', '.', ',']
+    
+    for intent in intents['intents']:
+        for pattern in intent['patterns']:
+            word_list = nltk.word_tokenize(pattern)
+            words.extend(word_list)
+            documents.append((word_list, intent['tag']))
+            if intent['tag'] not in classes:
+                classes.append(intent['tag'])
+    
+    words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_letters]
+    words = sorted(set(words))
+    classes = sorted(set(classes))
+    
+    pickle.dump(words, open('words.pkl', 'wb'))
+    pickle.dump(classes, open('classes.pkl', 'wb'))
+    
+    training = []
+    output_empty = [0] * len(classes)
+    
+    for document in documents:
+        bag = []
+        word_patterns = [lemmatizer.lemmatize(w.lower()) for w in document[0]]
+        for word in words:
+            bag.append(1) if word in word_patterns else bag.append(0)
+        output_row = list(output_empty)
+        output_row[classes.index(document[1])] = 1
+        training.append([bag, output_row])
+    
+    random.shuffle(training)
+    training = np.array(training, dtype=object)
+    train_x = np.array(list(training[:, 0]))
+    train_y = np.array(list(training[:, 1]))
+    
+    model = Sequential()
+    model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(len(train_y[0]), activation='softmax'))
+    
+    sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
+    model.save('chatbot_model.h5')
+
+# Load model dan data
 intents = json.loads(open('chatbot.json').read())
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
@@ -31,10 +92,8 @@ def get_bot_response(message):
     ERROR_THRESHOLD = 0.25
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
     results.sort(key=lambda x: x[1], reverse=True)
-    
     if not results:
         return "Maaf Kak, bot belum paham maksudnya. Bisa hubungi WA admin ya!"
-    
     tag = classes[results[0][0]]
     for i in intents['intents']:
         if i['tag'] == tag:
@@ -49,14 +108,12 @@ st.markdown("""
         flex-direction: row-reverse !important;
         background-color: transparent !important;
     }
-
     .stChatMessage:has([data-testid="stChatMessageAvatarUser"]) .stChatMessageContent {
         display: flex;
         flex-direction: column;
-        align-items: flex-end; 
+        align-items: flex-end;
         width: 100%;
     }
-
     .stChatMessage:has([data-testid="stChatMessageAvatarUser"]) p {
         text-align: right;
         width: 100%;
@@ -64,7 +121,7 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-st.image("https://raw.githubusercontent.com/KANSSSAAAHH/ProjekDTP/main/public/cottonfit-logo.png", width=100) 
+st.image("https://raw.githubusercontent.com/KANSSSAAAHH/ProjekDTP/main/public/cottonfit-logo.png", width=100)
 st.title("Smart Chat Assistant")
 st.write("Kami siap membantu mencari kaos asik untuk momen epikmu!")
 
@@ -79,12 +136,7 @@ if prompt := st.chat_input("Ketik pesan atau pertanyaan..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
-
     response = get_bot_response(prompt)
-    
     with st.chat_message("assistant"):
         st.markdown(response)
-        if "rekomendasi" in prompt.lower():
-            st.image("https://link-gambar-kaos.jpg", caption="Kaos Komunitas - Rp 85.000")
-
     st.session_state.messages.append({"role": "assistant", "content": response})
